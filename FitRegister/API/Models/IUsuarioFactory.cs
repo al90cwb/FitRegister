c1f675e60958463406bac0bc52cd2a6a1dd22257
al90cwb <162759789+al90cwb@users.noreply.github.com>
2024-10-09 12:39:44 -0300

    fix - remvido classes de teste para facotry (não utilizadas)

--- FitRegister/API/Models/IUsuarioFactory.cs
+++
@@ -1,9 +0,0 @@
-using System;
-
-namespace API.Models;
-
-public interface IUsuarioFactory
-{
-    Aluno CreateAluno(string nome, string endereco, string telefone, string login, string senha, PlanoDeUso plano, Professor professor);
-    Professor CreateProfessor(string nome, string endereco, string telefone, string login, string senha);
-}
--- FitRegister/API/Models/ProfessorUpdateDTO.cs
+++ FitRegister/API/Models/ProfessorUpdateDTO.cs
@@ -5,9 +5,9 @@ namespace API.Models;
 public class ProfessorUpdateDTO
 {
     public string Id { get; set; }
-    public string? Nome { get; set; }  // Nome pode ser null
-    public string? Endereco { get; set; }  // Endereco pode ser null
-    public string? Telefone { get; set; }  // Telefone pode ser null
-    public string? Login { get; set; }  // Login pode ser null
-    public string? Senha { get; set; }  // Senha pode ser null
+    public string? Nome { get; set; }
+    public string? Endereco { get; set; } 
+    public string? Telefone { get; set; }
+    public string? Login { get; set; } 
+    public string? Senha { get; set; } 
 }
