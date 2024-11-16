321b0e42710a558185fc9cd60a611b4123b3e845
al90cwb <162759789+al90cwb@users.noreply.github.com>
2024-11-16 16:47:19 -0300

    feat: Adicionado tema claro e o provider

--- FitRegister/front/src/App.tsx
+++ FitRegister/front/src/App.tsx
@@ -3,12 +3,16 @@ import { BrowserRouter } from "react-router-dom";
 import AlunoListar from "./pages/aluno/AlunoListar";
 import PlanoListar from "./pages/plano/PlanoListar";
 import { AppRoutes } from "./routes";
+import { ThemeProvider } from "@emotion/react";
+import { LightTheme } from "./shared/themes";
 
 function App() {
   return (
-    <BrowserRouter>
-      <AppRoutes/>
-    </BrowserRouter>
+    <ThemeProvider theme={LightTheme}>
+      <BrowserRouter>
+        <AppRoutes/>
+      </BrowserRouter>
+    </ThemeProvider>
   );
 }
 export default App;
