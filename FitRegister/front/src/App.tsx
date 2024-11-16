540cac09189956aec123350627c93ea9eaa7afb9
al90cwb <162759789+al90cwb@users.noreply.github.com>
2024-11-16 15:49:39 -0300

    feat: Adicionar React-router-dom

--- FitRegister/front/src/App.tsx
+++ FitRegister/front/src/App.tsx
@@ -1,13 +1,20 @@
 import React from "react";
+import { BrowserRouter } from "react-router-dom";
 import AlunoListar from "./pages/aluno/AlunoListar";
 import PlanoListar from "./pages/plano/PlanoListar";
+import { AppRoutes } from "./routes";
 
 function App() {
   return (
-    <div>
-      <PlanoListar />
-      <AlunoListar />
-    </div>
+    <BrowserRouter>
+      <AppRoutes/>
+      <div>
+        
+      </div>
+    </BrowserRouter>
   );
 }
 export default App;
+
+// <PlanoListar />
+// <AlunoListar />
